f3b2f237940fe2e4bf9dbac89e54b658a524bf0c
nikita10001 <n_ryabtsev1@mail.ru>
2023-10-02 20:36:10 +0300

    implement products serialization

---
+++ src/main/java/com/example/lab4fx/model/ProductList.java
@@ -0,0 +1,4 @@
+package com.example.lab4fx.model;
+
+public class ProductList {
+}
--- src/main/java/com/example/lab4fx/model/UserList.java
+++ src/main/java/com/example/lab4fx/model/UserList.java
@@ -1,24 +1,37 @@
 package com.example.lab4fx.model;
 
+import com.example.lab4fx.FileService;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class UserList {
-    List<User> users = new ArrayList<>(Arrays.asList(
-            new User("Nikita", "nikita", "nikita"),
-            new User("Admin", "admin", "admin"),
-            new User("User", "user", "user")
-    ));
+public class UserList implements Serializable {
+    //
+//    List<User> users = new ArrayList<>(Arrays.asList( //на случай удаления файла
+//            new User("Nikita", "nikita", "nikita"),
+//            new User("Admin", "admin", "admin"),
+//            new User("User", "user", "user")
+    List<User> users;
+    {
+        users = FileService.readFromFile(users, "users.txt");
+    }
     public UserList(List<User> users) {
         this.users = users;
+
     }
     public UserList(){}
 
     public void addUser(String name, String password, String login){
         users.add(new User(name, password, login));
+        FileService.writeInFile(users, "users.txt");
+//        showUsersList();
     }
+    public void getUsersFromFile() {
 
+        FileService.readFromFile(users, "users.txt");
+    }
     public Boolean isExistUser(String login, String password) {
         return users.stream() //есть ли user
                 .anyMatch(user ->
